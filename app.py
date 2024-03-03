3e7e63660183cae6d6a6f38c90d4a829b6173799
mag0451 <miguelgalban@my.unt.edu>
2024-03-02 20:51:50 -0600

    compression

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import base64
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.backends import default_backend
-
 app = Flask(__name__)
 @app.route("/.well-known/jwks.json", methods=["GET"])
 def jwks():
@@ -18,12 +17,10 @@ def jwks():
                 key_info["Public Key"],
                 backend=default_backend()
             )
-
             if isinstance(public_key, rsa.RSAPublicKey):
                 public_numbers = public_key.public_numbers()
                 modulus = public_numbers.n
                 exponent = public_numbers.e
-                
                 jwk = {
                     "kty": "RSA",
                     "use": "sig",
@@ -32,10 +29,8 @@ def jwks():
                     "n": base64.urlsafe_b64encode(modulus.to_bytes((modulus.bit_length() + 7) // 8, byteorder='big')).rstrip(b'=').decode('utf-8'),
                     "e": base64.urlsafe_b64encode(exponent.to_bytes((exponent.bit_length() + 7) // 8, byteorder='big')).rstrip(b'=').decode('utf-8'),
                 }
-                non_expired_keys.append(jwk)
-    
+                non_expired_keys.append(jwk)   
     return jsonify({"keys": non_expired_keys})
-
 @app.route("/auth", methods=["POST"])
 def auth():
     use_expired_key = "expired" in request.args
@@ -45,13 +40,9 @@ def auth():
             payload = {
                 "iss": "~~~migasso~~~",
                 "exp": datetime.utcnow() + timedelta(minutes=5),  # 5 minutes expiration
-                # Add more claims as needed
             }
             token = jwt.encode(payload, private_key, algorithm="RS256", headers={"kid": kid})
             return jsonify({"token": token})
-
     return jsonify({"error": "No suitable key found"}), 400
-
-
 if __name__ == "__main__":
     app.run(port=8080)
--- keyManager.py
+++ keyManager.py
@@ -3,9 +3,7 @@ from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
 import uuid
 from datetime import datetime, timedelta
-
 keysStorage = {}
-
 def generate_rsa_key_pair():
     privateKey = rsa.generate_private_key(public_exponent=65537, key_size=2048, backend=default_backend())
     expires = datetime.now() + timedelta(days=365)
@@ -13,11 +11,8 @@ def generate_rsa_key_pair():
     kid = str(uuid.uuid4())
     pemPrivate = privateKey.private_bytes(encoding=serialization.Encoding.PEM, format=serialization.PrivateFormat.PKCS8, encryption_algorithm=serialization.NoEncryption())
     pemPublic = publicKey.public_bytes(encoding=serialization.Encoding.PEM, format=serialization.PublicFormat.SubjectPublicKeyInfo)
-    
     keysStorage[kid] = {"Private Key": pemPrivate, "Public Key": pemPublic, "Expiry": expires}
-    
     return kid, pemPublic, expires
-
 kid, publicKey, expiry = generate_rsa_key_pair()
 print(f"Generated key with kid: {kid} and expiry: {expiry}")
 print("Public Key:", publicKey.decode())
